af4fc658dc0a5e39aa9936496ee822854de55fde
laura <lauradondiego@gmail.com>
2019-08-16 11:16:35 -0400

    initial commit, set up index.js in src and made reducer and actions folders with some content

---
+++ smurfs/src/reducers/smurfsReducer.js
@@ -0,0 +1,34 @@
+import {
+  FETCH_SMURF_DATA_START,
+  FETCH_SMURF_DATA_SUCCESS
+  // FETCH_WEBCAM_DATA_FAILURE
+} from "../actions";
+// or whatever makes sense for the api you choose
+// see if it is an array or whatever
+const initialState = {
+  smurfs: [],
+  isLoading: false,
+  error: ""
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_SMURF_DATA_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: ""
+      };
+    case FETCH_SMURF_DATA_SUCCESS:
+      console.log("action", action);
+      return {
+        ...state,
+        isLoading: false,
+        //   cats: action.payload.all,
+        smurfs: action.payload,
+        error: ""
+      };
+    default:
+      return state;
+  }
+};
